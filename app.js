a2e69511050534d3d6297ae0fe6c9cc9fc631f08
Braydon Coyer <braydon@coyer.net>
2020-05-22 11:07:21 -0500

    Allowing access to remove CORS issues

--- app.js
+++ app.js
@@ -6,6 +6,13 @@ const mongoose = require('mongoose');
 const config = require('./config/database');
 
 const app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 const apiRoutes = require('./routes/api-routes');
 app.use(bodyParser.urlencoded({
     extended: true
